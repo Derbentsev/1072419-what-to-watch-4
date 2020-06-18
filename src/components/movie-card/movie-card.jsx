4f873cb76c9949d393ec446c860dffa014a5d35a
Сергей Дербенцев <modcom@rambler.ru>
2020-06-18 18:26:04 +0300

    Доработал новые компоненты

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -7,15 +7,15 @@ export default class MovieCard extends PureComponent {
   }
 
   render() {
-    const {films} = this.props;
+    const {film} = this.props;
 
     return (
       <article className="small-movie-card catalog__movies-card">
         <div className="small-movie-card__image">
-          <img src="img/fantastic-beasts-the-crimes-of-grindelwald.jpg" alt="Fantastic Beasts: The Crimes of Grindelwald" width="280" height="175" />
+          <img src={`img/${film.image}`} alt={film.title} width="280" height="175" />
         </div>
         <h3 className="small-movie-card__title">
-          <a className="small-movie-card__link" href="movie-page.html">{films.title}</a>
+          <a className="small-movie-card__link" href="movie-page.html">{film.title}</a>
         </h3>
       </article>
     );
@@ -23,5 +23,5 @@ export default class MovieCard extends PureComponent {
 }
 
 MovieCard.propTypes = {
-  films: PropTypes.arrayOf(PropTypes.object).isRequired,
+  film: PropTypes.object.isRequired,
 };
---
+++ src/components/movies-list/movies-list.jsx
@@ -0,0 +1,28 @@
+import MovieCard from '@components/movie-card/movie-card';
+import {PureComponent} from 'react';
+
+
+export default class MoviesList extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const {films} = this.props;
+
+    return (
+      <div className="catalog__movies-list">
+        {films.map((film, i) => (
+          <MovieCard
+            key = {i}
+            film = {film}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+MoviesList.propTypes = {
+  films: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
