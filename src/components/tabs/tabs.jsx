7a9e4e825b68ce4050ceabfc42f225b6595d0cb3
Сергей Дербенцев <modcom@rambler.ru>
2020-06-26 18:51:13 +0300

    Добавил компонент tab-reviews

--- src/components/tabs/tabs.jsx
+++ src/components/tabs/tabs.jsx
@@ -1,12 +1,23 @@
 import TabOverview from '@components/tab-overview/tab-overview';
 import TabDetails from '@components/tab-details/tab-details';
+import TabReviews from '@components/tab-reviews/tab-reviews'
 
 
 export default class Tabs extends React.PureComponent {
   constructor(props) {
     super(props);
 
-    this.state = {activeTab: `Overview`};
+    this.state = {activeTab: `Reviews`};
+
+    this._handleOnTab = this._handleOnTab.bind(this);
+  }
+
+  _handleOnTab(evt) {
+    evt.preventDefault();
+
+    this.setState = {
+      activeTab: evt.target.textContent
+    };
   }
 
   render() {
@@ -14,32 +25,25 @@ export default class Tabs extends React.PureComponent {
 
     return (
       <>
-        <div className="movie-card__wrap movie-card__translate-top">
-          <div className="movie-card__info">
-            <div className="movie-card__poster movie-card__poster--big">
-              <img src="img/the-grand-budapest-hotel-poster.jpg" alt="The Grand Budapest Hotel poster" width="218" height="327" />
-            </div>
+        <div className="movie-card__desc">
+          <nav className="movie-nav movie-card__nav">
+            <ul className="movie-nav__list">
+              <li className={`movie-nav__item ${this.state.activeTab === `Overview` ? `movie-nav__item--active` : ``}`}>
+                <a href="#" onClick={this._handleOnTab} className="movie-nav__link">Overview</a>
+              </li>
+              <li className={`movie-nav__item ${this.state.activeTab === `Details` ? `movie-nav__item--active` : ``}`}>
+                <a href="#" onClick={this._handleOnTab} className="movie-nav__link">Details</a>
+              </li>
+              <li className={`movie-nav__item ${this.state.activeTab === `Reviews` ? `movie-nav__item--active` : ``}`}>
+                <a href="#" onClick={this._handleOnTab} className="movie-nav__link">Reviews</a>
+              </li>
+            </ul>
+          </nav>
 
-            <div className="movie-card__desc">
-              <nav className="movie-nav movie-card__nav">
-                <ul className="movie-nav__list">
-                  <li className="movie-nav__item movie-nav__item--active">
-                    <a href="#" className="movie-nav__link">Overview</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Details</a>
-                  </li>
-                  <li className="movie-nav__item">
-                    <a href="#" className="movie-nav__link">Reviews</a>
-                  </li>
-                </ul>
-              </nav>
+          {this.state.activeTab === `Overview` && <TabOverview/>}
+          {this.state.activeTab === `Details` && <TabDetails/>}
+          {this.state.activeTab === `Reviews` && <TabReviews/>}
 
-              {this.state.activeTab === `Overview` && <TabOverview/>}
-              {this.state.activeTab === `Details` && <TabDetails/>}
-
-            </div>
-          </div>
         </div>
       </>
     );
