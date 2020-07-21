56bddfd5df9f6e8204f8a014644f4c04bf83f892
Сергей Дербенцев <modcom@rambler.ru>
2020-07-21 15:18:33 +0300

    Добавил proptypes в хоки

--- src/hocs/with-movies-list/with-movies-list.jsx
+++ src/hocs/with-movies-list/with-movies-list.jsx
@@ -26,6 +26,32 @@ const withMoviesList = (Component) => {
     }
   }
 
+  WithMoviesList.propTyprs = {
+    films: PropTypes.arrayOf(PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      poster: PropTypes.string.isRequired,
+      genre: PropTypes.string.isRequired,
+      dateRelease: PropTypes.number.isRequired,
+      cover: PropTypes.string.isRequired,
+      videoSrc: PropTypes.string.isRequired,
+      previewVideoSrc: PropTypes.string.isRequired,
+      director: PropTypes.string.isRequired,
+      actors: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+      ratingScore: PropTypes.number.isRequired,
+      ratingCount: PropTypes.number.isRequired,
+      description: PropTypes.string.isRequired,
+      runTime: PropTypes.number.isRequired,
+      reviews: PropTypes.arrayOf(PropTypes.shape({
+        comment: PropTypes.string.isRequired,
+        rating: PropTypes.number.isRequired,
+        author: PropTypes.string.isRequired,
+        date: PropTypes.string.isRequired,
+      })),
+    })).isRequired,
+    handleMovieCardMouseEnter: PropTypes.func.isRequired,
+  };
+
   return WithMoviesList;
 };
 
